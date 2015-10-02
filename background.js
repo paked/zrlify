ce8d29e0ea163a8d29eb6cdea311adeb95b60ad8
Harrison <harrison@theshoebridges.com>
2015-10-02 18:30:18 +1000

    Add documentation

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
+// Run zrlify when the popup icon is pressed.
 chrome.browserAction.onClicked.addListener(function(tab) {
-    console.log("selectin");
-
     chrome.tabs.getSelected(null, function(tab){
         chrome.tabs.executeScript(tab.id, {code: 'window.ZRLIFY();'},
                 function(response) {
--- zrlify.js
+++ zrlify.js
@@ -2,10 +2,12 @@ console.log("zrlifying");
 
 var zrls = [];
 
+// Generate zrl image URLs
 for (var i = 0; i < 5; i++) {
     zrls.push("https://alexb.io/f/zach" + (i + 1) + ".jpg")
 }
 
+// addZrlify creates a MutationObserver to watch the DOM and update elements which do not conform to style
 function addZrlify() {
     var observer = new window.WebKitMutationObserver(function(mutations, observer) {
         zrlify();
@@ -22,7 +24,8 @@ var ascii = /^[ -~]*$/;
 
 function zrlify() {
     var images = document.getElementsByTagName("img");
-
+    
+    // Iterate over images in the web page and perform replacements
     for (var i = 0; i < images.length; i++) {
         var image = images[i];
 
@@ -30,9 +33,10 @@ function zrlify() {
         var h = image.height;
 
         image.src = zrls[randInt(0, zrls.length)];
+        
+        // Make sure that the image does not look too out of place by scaling it.
         image.width = w;
         image.height = h;
-
         image.style.width = w + "px";
         image.style.height = h + "px";
     }
@@ -45,19 +49,22 @@ function zrlify() {
         }
 
         el.style.fontFamily = "Times, \"Times New Roman\", serif";
-
+        
+        // If the element is a octicon or equivalent, do not apply a font.
         var content = window.getComputedStyle(el, ':before')
         var text = content.getPropertyValue('content')
         if (!ascii.test(text)) {
             el.style.fontFamily = "";
         }
-
+        
+        // Make links look normal-ish
         if (el.tagName == "A") {
             el.style.color = "blue";
         } else {
             el.style.color = "black";
         }
-
+        
+        // Various style resets.
         el.style.background = "none";
         el.style.backgroundColor = "white";
         el.style.borderColor = "black";
@@ -70,8 +77,10 @@ function randInt(min, max) {
     return Math.floor((Math.random() * max) + min);
 }
 
+// Whether zrlify has been executed or not
 window.ZRLIFY_SET = false;
 
+// The entry point for zrlify
 window.ZRLIFY = function() {
     if (!window.ZRLIFY_SET) {
         zrlify();

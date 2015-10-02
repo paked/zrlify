e1ce2f01cd773fd55a3282c067136632970501e8
Harrison <harrison@theshoebridges.com>
2015-10-02 16:50:50 +1000

    Add @ZucchiniZe's images

--- zrlify.js
+++ zrlify.js
@@ -2,15 +2,11 @@ MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 console.log("zrlifying");
 
-var zrls = ["https://hatchexperience.org/wp-content/uploads/gravity_forms/8-2c08cc885a433151d8e43ebd3a8a0e84/2015/08/orbit_profile.jpg",
-"http://d.fastcompany.net/multisite_files/fastcompany/imagecache/slideshow_large/slideshow/2015/06/3047076-slide-s-18-thiel-fellows-zach-latta.jpg",
-"http://res.cloudinary.com/dsjid8mlr/image/upload/c_thumb,f_auto,g_faces,h_600,w_600/v1421540399/ney0aexerkxplxhbjeap.jpg"];
+var zrls = [];
 
-
-/*document.addEventListener("DOMContentLoaded", function() {
-    zrlify();
-    addZrlify();
-});*/
+for (var i = 0; i < 5; i++) {
+    zrls.push("https://alexb.io/f/zach" + (i + 1) + ".jpg")
+}
 
 function addZrlify() {
     var observer = new window.WebKitMutationObserver(function(mutations, observer) {
